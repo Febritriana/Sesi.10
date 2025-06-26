94fdc728271ae2cc9a15ce3a1adbacb3f87c7e87
Febritriana <febri.triana19@gmail.com>
2025-06-26 18:53:41 +0700

    Add node_modules folder node_modules

---
+++ tests/Sesi-9.js
@@ -0,0 +1,50 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
+
+describe('SauceDemo Automation', function () {
+  let driver;
+
+it('Sukses Login', async function(){
+  driver = await new Builder().forBrowser('chrome').build();
+
+  await driver.get('https://www.saucedemo.com');
+
+  let inputUsername = await driver.findElement(By.css('[data-test="username"]'))
+  let inputPassword = await driver.findElement(By.xpath('//*[@data-test="password"]'))
+  let buttonLogin = await driver.findElement(By.className('submit-button btn_action'))
+  await inputUsername.sendKeys('standard_user')
+  await inputPassword.sendKeys('secret_sauce')
+  await buttonLogin.click()
+
+  const pageTitle = await driver.findElement(By.className('title')).getText();
+  assert.strictEqual(pageTitle, 'Products');
+
+  await driver.wait(until.elementLocated(By.className('title')),);
+  const titleText = await driver.findElement(By.className('title')).getText();
+  assert.strictEqual(titleText, 'Products');
+
+})
+
+it('Urutkan Produk dari Z ke A', async function () {
+    await driver.wait(until.elementLocated(By.className('product_sort_container')));
+    
+     await driver.wait(until.elementLocated(By.className('product_sort_container')));
+    const sortDropdown = await driver.findElement(By.className('product_sort_container'));
+    await sortDropdown.sendKeys('Name (Z to A)');
+
+     const productElements = await driver.findElements(By.className('inventory_item_name'));
+    const productNames = [];
+
+    for (let element of productElements) {
+      const name = await element.getText();
+      productNames.push(name);
+    }
+
+    const sortedNames = [...productNames].sort().reverse();
+    assert.deepStrictEqual(productNames, sortedNames);
+  
+ await driver.quit();
+
+
+})
+  });
